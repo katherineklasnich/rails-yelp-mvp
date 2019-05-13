96044d25cbfa33725457d87b7f0e6b071a6d6b8e
Katherine Klasnich <katherineklasnich@gmail.com>
2019-05-13 12:56:26 +0100

    seeded data and added controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#   RestaurantsController
+#   routing
+#     routes to #index (FAILED - 15)
+#     routes to #new (FAILED - 16)
+#     routes to #show (FAILED - 17)
+#     routes to #create (FAILED - 18)
+
+# ReviewsController
+#   routing
+#     routes to #new (FAILED - 19)
+#     routes to #create (FAILED - 20)
+
 end
