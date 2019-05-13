e640cdd57f638ff654256b3128fa2a53d7da5775
Katherine Klasnich <katherineklasnich@gmail.com>
2019-05-13 15:06:11 +0100

    added actions to restaurants and reviews controller. Created show, index, and new views in restaurants views folder

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
 #   RestaurantsController
 #   routing
 #     routes to #index (FAILED - 15)
+  get "restaurants", to: "restaurants#index"
 #     routes to #new (FAILED - 16)
+  get "restaurants/new", to: "restaurants#new"
 #     routes to #show (FAILED - 17)
+  get "restaurants/:id", to: "restaurants#show"
 #     routes to #create (FAILED - 18)
+  post "restaurants", to: "restaurants#create"
 
 # ReviewsController
 #   routing
 #     routes to #new (FAILED - 19)
+  get "reviews", to: "reviews#new"
 #     routes to #create (FAILED - 20)
+  post "reviews", to: "reviews#create"
 
 end
